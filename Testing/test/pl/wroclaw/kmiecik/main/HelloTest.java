37333be4fc99a0d3d34a16a4c2d4ab34f6ebb708
anetakmiecik <aneta.kmiecik@capgemini.com>
2018-11-04 21:39:03 +0100

    JUnit test for Hello.addNumbers

---
+++ Testing/test/pl/wroclaw/kmiecik/main/HelloTest.java
@@ -0,0 +1,20 @@
+package pl.wroclaw.kmiecik.main;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloTest {
+	
+	@Test
+	public void shouldAddNumbers() {
+		// given
+		int firstArg = 8;
+		int secondArg = 2;
+		// when
+		int sum = Hello.addNumbers(firstArg, secondArg);
+		// then
+		assertTrue(sum == firstArg + secondArg);
+	}
+	
+}
