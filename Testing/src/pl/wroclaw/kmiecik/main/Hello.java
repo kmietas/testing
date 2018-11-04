3deb9fd51891831893d00678f83e6688893ff20b
Marcin Kmiecik <marcin@kmiecik.wroclaw.pl>
2018-11-04 21:24:42 +0100

    Added Main and Hello

---
+++ Testing/src/pl/wroclaw/kmiecik/main/Hello.java
@@ -0,0 +1,8 @@
+package pl.wroclaw.kmiecik.main;
+
+public class Hello {
+	public static int addNumbers(int first, int second) {
+		return first + second;
+	}
+	
+}
---
+++ Testing/src/pl/wroclaw/kmiecik/main/Main.java
@@ -0,0 +1,10 @@
+package pl.wroclaw.kmiecik.main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println(Hello.addNumbers(1, 2));
+		
+	}
+	
+}
